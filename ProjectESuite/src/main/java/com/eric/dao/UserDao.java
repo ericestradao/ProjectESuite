b88224713b6a32b573670d86be4161a7c3129f24
Eric Estrada <Eric Estrada@DESKTOP-J62LONE>
2019-07-18 10:14:37 -0400

    JWTDB security added

---
+++ ProjectESuite/src/main/java/com/eric/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.eric.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eric.model.DAOUser;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+	
+	DAOUser findByUsername(String username);
+	
+}
